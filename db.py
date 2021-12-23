95f33c29064ec7f4d9d5f32d94a89a5b5a3fbafa
Gordian Basili <55690016+digitalbunny@users.noreply.github.com>
2021-12-23 17:36:06 +0300

    setup db

--- db.py
+++ db.py
@@ -1,1 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, join
+from sqlalchemy.ext.automap import automap_base
 
+SQLALCHEMY_DATABASE_URL = "mysql://Zuno:ZunoInterview@zunolibrary.cv07g5ggoiyj.us-east-2.rds.amazonaws.com/library"
+
+Base = automap_base()
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Reflect existing database tables
+Base.prepare(engine, reflect=True)
+
+# Create mapped classes matching database table names
+Media = Base.classes.media
+MediaType = Base.classes.media_type
+CheckoutData = Base.classes.checkout_data
+
+session = Session(engine)

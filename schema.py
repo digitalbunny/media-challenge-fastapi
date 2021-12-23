67e10cceb634443044fcb8ea82efcf9cf1a57b0c
Gordian Basili <55690016+digitalbunny@users.noreply.github.com>
2021-12-23 17:36:31 +0300

    setup schema

--- schema.py
+++ schema.py
@@ -1,1 +1,55 @@
+from typing import List, Optional
+from pydantic import BaseModel
 
+
+class Media(BaseModel):
+    BibNum: int
+    Title: str
+    Author: str
+    ISBN: Optional[str]
+    PublicationYear: int
+    Publisher: str
+    Subjects: str
+    ItemType: str
+    ItemCount: int
+
+    class Config:
+        orm_mode = True
+
+
+class MediaType(BaseModel):
+    Code: str
+    CodeType: str
+    Description: str
+    FormatGroup: str
+    FormatSubgroup: Optional[str]
+    CategoryGroup: Optional[str]
+    CategorySubgroup: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+
+class CheckoutData(BaseModel):
+    id: int
+    BibNumber: int
+    ItemBarcode: int
+    ItemType: str
+    CallNumber: str
+    CheckoutDateTime: str
+
+    class Config:
+        orm_mode = True
+
+
+class MediaResponse(BaseModel):
+    count: int
+    offset: int
+    limit: int
+    results: List[Media]
+
+
+class MediaDetailResponse(BaseModel):
+    source: Media
+    source_type: MediaType
+    checkout: List[CheckoutData]
